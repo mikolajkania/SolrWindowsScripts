57e9b0afcbeb953bdf140a8545d8b32ad64f0fd6
Mikołaj <mikolajkania@users.noreply.github.com>
2016-02-07 19:42:19 +0100

    Additional options for heap dump, new java path

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,6 +1,7 @@
-SET JAVA_HOME=C:\Program Files\Java\jre1.8.0_60
+SET JAVA_HOME=C:\Program Files\Java\jre1.8.0_71
 
 SET SOLR_SERVER=C:\Code\solr-5.3.1
 SET SOLR_HOME=C:\Code\solr-data\techproducts
 
-%SOLR_SERVER%/bin/solr start -V -s %SOLR_HOME% -a "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
+# start in foreground, show messages, set solr home, set java parameters - debug, creating heap dump when memory problems appear
+%SOLR_SERVER%/bin/solr start -f -V -s %SOLR_HOME% -a "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -XX:+HeapDumpOnOutOfMemoryError"
